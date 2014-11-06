c5385d566bfb0d45cbb8c5d6dc971abeb8865312
birkin <birkin.diana@gmail.com>
2014-11-06 16:46:15 -0500

    unit-tests z39 lib.

--- utils/test_avail.py
+++ utils/test_avail.py
@@ -1,30 +1,72 @@
-import json, logging
+# -*- coding: utf-8 -*-
+
+import json, logging, unittest
 from backend import Search
 
 
-formatter = logging.Formatter(u'%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter( u'%(asctime)s - %(levelname)s - %(message)s' )
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel( logging.INFO )
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+console_handler.setFormatter( formatter )
+logger.addHandler( console_handler )
+
+
+class z39Test( unittest.TestCase ):
+    """ Tests brown fork of PyZ3950. """
+
+    def setUp(self):
+        self.z39 = Search( logger )
+        self.book_bib = 'b3386235'
+        self.journal_bib = 'b4074295'
+        self.online_journal_bib = 'b7091233'
+
+    def tearDown(self):
+        self.z39.close()
+
+    def test_z39lib_unicode_param(self):
+        """ Tests lib unicode handling. """
+        try:
+            rsp = self.z39.id( unicode(self.book_bib) )
+        except Exception as e:
+            expected = u"<class 'PyZ3950.zoom.QuerySyntaxError'>"
+            self.assertEqual( expected, unicode(type(e)) )
+
+    def test_z39_book(self):
+        """ Tests returned book data. """
+        ## service returns list
+        rsp = self.z39.id( self.book_bib )
+        # print( json.dumps(rsp, indent=2) )
+        expected = list
+        self.assertEqual( expected, type(rsp) )
+        ## list of dicts
+        item = rsp[0]
+        expected = dict
+        self.assertEqual( expected, type(item) )
+        expected = ['barcodes', 'id', 'isbn', 'items', 'lccn', 'oclc', 'summary', 'title', 'url']
+        self.assertEqual( expected, sorted(item.keys()) )
+        ## id reflected back
+        expected = self.book_bib
+        self.assertEqual( expected, item[u'id'] )
+        ## tho utf-strings required as param, returned values are unicode
+        self.assertEqual( unicode, type(item[u'id']) )
+
+    def test_z39_journal(self):
+        """ Tests returned journal data. """
+        rsp = self.z39.id( self.journal_bib )
+        item = rsp[0]
+        expected = ['barcodes', 'id', 'isbn', 'items', 'lccn', 'oclc', 'summary', 'title', 'url']
+        self.assertEqual( expected, sorted(item.keys()) )
+
+    def test_z39_online_journal(self):
+        """ Tests returned online-journal data. """
+        rsp = self.z39.id( self.online_journal_bib )
+        item = rsp[0]
+        expected = ['barcodes', 'id', 'isbn', 'items', 'lccn', 'summary', 'title', 'url']
+        self.assertEqual( expected, sorted(item.keys()) )
 
 
 
 if __name__ == "__main__":
-
-    z39 = Search( logger )
-
-    #ToDo: collect test items
-    book = 'b3386235'
-    journal = 'b4074295'
-    online_journal = 'b7091233'
-
-    rsp = z39.id(online_journal)
-
-    print(json.dumps(rsp, indent=2))
-
-    print len(rsp[0]['items'])
-    print len(rsp[0]['barcodes'])
-
-    z39.close()
+  unittest.TestCase.maxDiff = None  # allows error to show in long output
+  unittest.main()
