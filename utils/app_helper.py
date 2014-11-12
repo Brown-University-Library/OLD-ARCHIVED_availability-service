2a58a90225509030152d7f7e062b71c02c8f83c0
birkin <birkin.diana@gmail.com>
2014-11-12 13:59:57 -0500

    adds show-marc support.

--- utils/app_helper.py
+++ utils/app_helper.py
@@ -19,16 +19,17 @@ class HandlerHelper( object ):
         self.cache_dir = os.getenv( u'availability_CACHE_DIR' )
         self.cache_minutes = int( os.getenv(u'availability_CACHE_MINUTES') ) * 60  # timeout param requires seconds
 
-    def build_query_dict( self, url, key, value ):
+    def build_query_dict( self, url, key, value, show_marc_param ):
         """ Query reflector.
             Called by availability_service.availability_app.handler(). """
         start_time = datetime.datetime.now()
         query_dict = {
             u'url': url,
             u'query_timestamp': unicode(start_time),
-            u'service_id': key,
-            u'service_value': value
-            }
+            u'query_key': key,
+            u'query_value': value, }
+        if show_marc_param == u'true':
+            query_dict[u'show_marc'] = show_marc_param
         return query_dict
 
     def validate( self, key, value ):
@@ -42,44 +43,28 @@ class HandlerHelper( object ):
         self.log.debug( u'in validate(); message, %s' % message )
         return message
 
-    def build_response_dict( self, key, value ):
+    def build_response_dict( self, key, value, show_marc_param ):
         """ Handler for cached z39.50 call and response.
             Called by availability_service.availability_app.handler(). """
         assert type(value) == unicode
         cache = FileSystemCache( self.cache_dir, threshold=500, default_timeout=self.cache_minutes, mode=0664 )  # http://werkzeug.pocoo.org/docs/0.9/contrib/cache/
-        cache_key = u'%s_%s' % ( key, value )
+        cache_key = u'%s_%s_%s' % ( key, value, show_marc_param )
         response_dict = cache.get( cache_key )
         if response_dict is None:
-            response_dict = self.query_josiah( key, value )
-            # response_dict = self.query_josiah( key, value.encode(u'utf-8') )
+            response_dict = self.query_josiah( key, value, show_marc_param )
             cache.set( cache_key, response_dict )
         return response_dict
 
-    def query_josiah( self, key, value ):
+    def query_josiah( self, key, value, show_marc_param ):
         """ Perform actual query.
             Called by self.build_response_dict(). """
+        marc_flag = True if show_marc_param == u'true' else False
         srchr = z3950_wrapper.Searcher( self.log )
         srchr.connect()
-        item_list = srchr.search( key, value, marc_flag=False )
+        item_list = srchr.search( key, value, marc_flag )
         srchr.close_connection()
         return {
             u'backend_response': item_list,
             u'response_timestamp': unicode(datetime.datetime.now()) }
-
-    # def query_josiah( self, key, utf8_value ):
-    #     """ Perform actual query.
-    #         Called by self.build_response_dict(). """
-    #     from availability_service.utils import backend
-    #     z39 = backend.Search( self.log )
-    #     if key == u'bib':
-    #         rsp_list = z39.id( utf8_value )
-    #     elif key == u'isbn':
-    #         rsp_list = z39.isbn( utf8_value )
-    #     elif key == u'issn':
-    #         rsp_list = z39.issn( utf8_value )
-    #     elif key == u'oclc':
-    #         rsp_list = z39.oclc( utf8_value )
-    #     z39.close()
-    #     return { u'backend_response': unicode(repr(rsp_list)), u'response_timestamp': unicode(datetime.datetime.now()) }
 
     # end class HandlerHelper
